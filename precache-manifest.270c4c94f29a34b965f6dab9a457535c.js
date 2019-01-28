1ceda19937cd00fed3d6b84fb817fffb36a23680
yoon <ynk.mv@daum.net>
2019-01-28 16:36:56 +0900

    Updates

---
+++ precache-manifest.270c4c94f29a34b965f6dab9a457535c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "1078149188aa842a6f6c",
+    "url": "/movie_app/static/js/main.10781491.chunk.js"
+  },
+  {
+    "revision": "d120fdf0ddba1865b329",
+    "url": "/movie_app/static/js/1.d120fdf0.chunk.js"
+  },
+  {
+    "revision": "1078149188aa842a6f6c",
+    "url": "/movie_app/static/css/main.fefd6810.chunk.css"
+  },
+  {
+    "revision": "1301e8816e363fab3c06e4b74a7de025",
+    "url": "/movie_app/index.html"
+  }
+];
